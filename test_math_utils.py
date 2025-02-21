f695064894fa31c1d45cf3d8d45267cd26fa20bd
Francescaparty <Francescaparticelli6@gmail.com>
2025-02-21 16:09:56 +0100

    Modify 1610
    Adding another python file test_math_utils

---
+++ test_math_utils.py
@@ -0,0 +1,34 @@
+import unittest
+from math_utils import MathUtils
+
+class TestMathUtils(unittest.TestCase):
+    """
+    Test Class for MathUtils.
+    """
+
+    def test_add(self):
+        """
+        Just adds two numbers together.
+        """
+        self.assertEqual(MathUtils.add(1, 2), 3)
+        self.assertEqual(MathUtils.add(1.5, 2.5), 4.0)
+        self.assertEqual(MathUtils.add(-1, 1), 0)
+        self.assertEqual(MathUtils.add(-1.5, 1.5), 0)
+
+
+    def subtract(self):
+        """
+        Just subtracts two numbers.
+        """
+        self.assertEqual(MathUtils.subtract(1, 2), -1)
+        self.assertEqual(MathUtils.subtract(1.5, 2.5), -1.0)
+        self.assertEqual(MathUtils.subtract())
+        self.assertEqual(MathUtils.subtract())
+
+
+    def divide(self):
+        """
+        Just divides two numbers.
+        """
+        self.assertEqual(MathUtils.divide(3, 5), 4)
+        self.assertEqual(MathUtils.divide)
